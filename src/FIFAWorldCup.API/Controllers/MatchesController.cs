770b3b5456a465e5e5a4a023a82462ba0e8150a8
Armin Shoeibi <a.shoeibi@alibaba.ir>
2022-11-24 03:17:48 +0330

    Add GetMatches action method

---
+++ src/FIFAWorldCup.API/Controllers/MatchesController.cs
@@ -0,0 +1,19 @@
+﻿using FIFAWorldCup.API.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FIFAWorldCup.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class MatchesController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<List<MatchDto>> GetMatches()
+    {
+        return Ok(new List<MatchDto>()
+        {
+            new(3, "England", "Iran", new DateTimeOffset(2022, 11, 20, 16, 0, 0, new TimeSpan(3, 0, 0))),
+            new(17, "Iran", "Wales", new DateTimeOffset(2022, 11, 25, 13, 0, 0, new TimeSpan(3, 0, 0))),
+            new(34, "Iran", "USA", new DateTimeOffset(2022, 11, 29, 22, 0, 0, new TimeSpan(3, 0, 0))),
+        });
+    }
+}
---
+++ src/FIFAWorldCup.API/DTOs/MatchDto.cs
@@ -0,0 +1,3 @@
+﻿namespace FIFAWorldCup.API.DTOs;
+
+public sealed record class MatchDto(byte MatchNumber, string TeamAName, string TeamBName, DateTimeOffset MatchDateTime);
